4bc3d3058c607b5997585bcfa557aa1a3149a817
Sasha Di Ventura <diventura.sasha@gmail.com>
2023-06-18 13:38:43 -0400

    ajustando API

--- pokeapi.js
+++ pokeapi.js
@@ -7,8 +7,8 @@ fetch(url)
     .then(response => response.json())
     .then(data => pokeMostrar(data))
 
+// FUNCIÓN PARA OBTENER LA DATA DEL POKÉMON Y MOSTRAR EN LA PÁGINA
 function pokeMostrar(pokeData) {
-
     let pokeTipos = pokeData.types.map((type) => ` 
                                             <li class="list-group-item ${type.type.name}"> Tipos: ${type.type.name}</li>  `)
     pokeTipos = pokeTipos.join("")
@@ -36,41 +36,3 @@ function pokeMostrar(pokeData) {
                     `
     pokeShow.append(div)
 }
-
-
-
-/*
-
-<div class="card mt-2" style="max-width: 100;">
-    <div class="row g-0">
-        <div class="col-md-4">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/94.png"
-                class="img-fluid rounded-start" alt="...">
-        </div>
-        <div class="col-md-8">
-            <div class="card-body">
-                <h5 id="pokeName" class="card-title text-uppercase">GENGAR</h5>
-                <p id="pokeDesc" class="card-text">Gengar es un Pokémon de aspecto sombrío y sonrisa
-                    siniestra. Se desliza a través de las paredes y asusta a los demás.</p>
-                <ul class="list-group">
-                    <li id="pokeType" class="list-group-item">Tipo: </li>
-                    <li id="pokeHeight" class="list-group-item">Altura: </li>
-                    <li id="pokeWeight" class="list-group-item">Peso: </li>
-                </ul>
-                <p id="pokeId" class="pt-5 card-text"><small class="text-body-secondary">PokéAPI ID:
-                        94</small></p>
-            </div>
-        </div>
-    </div>
-</div>
-
-*/
-
-
-
-    // fetch("./catalogo.json")
-    // .then(response => response.json())
-    // .then(data => {
-    //     catalogo = data
-    //     cargarJuegos(catalogo)
-    // })
